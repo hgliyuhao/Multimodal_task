94f037d389f0aeb5911f093971184521a1d25e9f
hgliyuhao <1241225413@qq.com>
2022-08-29 17:04:24 +0800

    修改格式

--- muti_model.py
+++ muti_model.py
@@ -16,56 +16,55 @@ from bert4keras.snippets import sequence_padding, DataGenerator
 from net import *
 import cv2
 
-import random 
-random.seed(1) 
+import random
+random.seed(1)
 set_gelu('tanh')  # 切换gelu版本
 maxlen = 32
 batch_size = 32
 p = 'chinese_roberta_wwm_ext_L-12_H-768_A-12/'
-config_path = p +'bert_config.json'
+config_path = p + 'bert_config.json'
 checkpoint_path = p + 'bert_model.ckpt'
-dict_path = p +'vocab.txt'
+dict_path = p + 'vocab.txt'
 tokenizer = Tokenizer(dict_path, do_lower_case=True)
 all_category = set()
 
+
 def read_img(img_path):
-    
     """
         涉及到后面图片模块的处理,因此img_size定为固定值
         单图读取函数（对非方形的图片进行白色填充，使其变为方形）
     """
-    
+
     img_size = 224
     img = cv2.imread(img_path)
     height, width = img.shape[:2]
     if height > width:
-            height, width = img_size, width * img_size // height
-            img = cv2.resize(img, (width, height))
-            delta = (height - width) // 2
-            img = cv2.copyMakeBorder(
-                img,
-                top=0,
-                bottom=0,
-                left=delta,
-                right=height - width - delta,
-                borderType=cv2.BORDER_CONSTANT,
-                value=[255, 255, 255]
-            )
+        height, width = img_size, width * img_size // height
+        img = cv2.resize(img, (width, height))
+        delta = (height - width) // 2
+        img = cv2.copyMakeBorder(
+            img,
+            top=0,
+            bottom=0,
+            left=delta,
+            right=height - width - delta,
+            borderType=cv2.BORDER_CONSTANT,
+            value=[255, 255, 255])
     else:
-            height, width = height * img_size // width, img_size
-            img = cv2.resize(img, (width, height))
-            delta = (width - height) // 2
-            img = cv2.copyMakeBorder(
-                img,
-                top=delta,
-                bottom=width - height - delta,
-                left=0,
-                right=0,
-                borderType=cv2.BORDER_CONSTANT,
-                value=[255, 255, 255]
-            )
+        height, width = height * img_size // width, img_size
+        img = cv2.resize(img, (width, height))
+        delta = (width - height) // 2
+        img = cv2.copyMakeBorder(
+            img,
+            top=delta,
+            bottom=width - height - delta,
+            left=0,
+            right=0,
+            borderType=cv2.BORDER_CONSTANT,
+            value=[255, 255, 255])
     img = img.astype('float32')
-    return img[..., ::-1] # cv2的读取模式为BGR，但keras的模型要求为RGB
+    return img[..., ::-1]  # cv2的读取模式为BGR，但keras的模型要求为RGB
+
 
 def read_data(path):
 
@@ -77,11 +76,11 @@ def read_data(path):
         text = i['text']
         label = i['label']
         img_path = i['img_path']
-        
-        data.append([text,label,img_path])
+
+        data.append([text, label, img_path])
         all_category.add(label)
-    
-    return data         
+
+    return data
 
 
 train_data = read_data('train_data/train.json')
@@ -90,11 +89,13 @@ val_data = read_data('train_data/val.json')
 all_category = list(all_category)
 all_category.sort()
 
-id2label,label2id = fa.label2id(all_category)
+id2label, label2id = fa.label2id(all_category)
+
 
 class data_generator(DataGenerator):
     """数据生成器
     """
+
     def __iter__(self, random=False):
         idxs = list(range(len(self.data)))
         if random:
@@ -115,12 +116,12 @@ class data_generator(DataGenerator):
                 batch_token_ids = sequence_padding(batch_token_ids)
                 batch_segment_ids = sequence_padding(batch_segment_ids)
                 batch_labels = sequence_padding(batch_labels)
-                yield [batch_token_ids, batch_segment_ids, batch_images], batch_labels
+                yield [batch_token_ids, batch_segment_ids,
+                       batch_images], batch_labels
                 batch_images, batch_token_ids, batch_segment_ids,batch_labels = [], [], [], []
 
-cv_model = build_cv_model(
-    model_name = "resnet50"
-)
+
+cv_model = build_cv_model(model_name="resnet50")
 
 cv_inputs = cv_model.input
 cv_output = cv_model.output
@@ -128,26 +129,24 @@ cv_output = cv_model.output
 bert = build_transformer_model(
     config_path,
     checkpoint_path,
-    layer_norm_cond= cv_output,
+    layer_norm_cond=cv_output,
     layer_norm_cond_hidden_size=128,
     layer_norm_cond_hidden_act='swish',
-    additional_input_layers= cv_inputs,
+    additional_input_layers=cv_inputs,
 )
 
-output = Lambda(lambda x: x[:, 0],
-                name='CLS-token')(bert.output)
+output = Lambda(lambda x: x[:, 0], name='CLS-token')(bert.output)
 
-output = Dense(len(all_category),activation='softmax')(output)
+output = Dense(len(all_category), activation='softmax')(output)
 
 model = Model(bert.inputs, output)
 model.compile(
-
     loss='sparse_categorical_crossentropy',
     optimizer=Adam(2e-5),  # 用足够小的学习率
     metrics=['accuracy'],
-    
 )
 model.summary()
+
 
 def evaluate(data):
     total, right = 0., 0.
@@ -172,8 +171,8 @@ class Evaluator(keras.callbacks.Callback):
         if val_acc > self.best_val_acc:
             self.best_val_acc = val_acc
             model.save_weights('model/muti_model.weights')
-        print(u'val_acc: %.5f, best_val_acc: %.5f, test_acc: %.5f\n'
-              % (val_acc, self.best_val_acc, 0))
+        print(u'val_acc: %.5f, best_val_acc: %.5f, test_acc: %.5f\n' %
+              (val_acc, self.best_val_acc, 0))
 
 
 if __name__ == '__main__':
@@ -186,6 +185,4 @@ if __name__ == '__main__':
         train_generator.forfit(),
         steps_per_epoch=len(train_generator),
         epochs=40,
-        callbacks=[evaluator]
-    )
-    
+        callbacks=[evaluator])
